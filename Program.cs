e67debeccf207a778a2fcd4330a69ec1850d8ba0
Hariharan Subramanian <hari@flowinsights.com>
2022-12-10 14:20:05 +0100

    Counting scenic score using LINQ

--- Program.cs
+++ Program.cs
@@ -1,77 +1,58 @@
 ﻿Console.WriteLine("Analysing tree grid...");
 
-// var lines = File.ReadAllLines("tree_grid.txt");
-var lines = File.ReadAllLines("test_input.txt");
-
+var lines = File.ReadAllLines("tree_grid.txt");
 var treeGrid = lines.Select(line =>
 {
     var numbers = line.Select(c => (int) char.GetNumericValue(c)).ToList();
     return numbers;
 }).ToList();
 
+// Puzzle 1
 var visibleTreeCount = 0;
 
-for (var i = 0; i < treeGrid.Count; i++)
+for (var row = 0; row < treeGrid.Count; row++)
+for (var col = 0; col < treeGrid[row].Count; col++)
 {
-    for (var j = 0; j < treeGrid[i].Count; j++)
-    {
-        var treeHeight = treeGrid[i][j];
-        if (i == 0 || j == 0 || i == treeGrid.Count - 1 || j == treeGrid[i].Count - 1) visibleTreeCount++; // edge detected
-        else
-        {
-            var isVisibleFromLeft = true;
-            var isVisibleFromRight = true;
-            var isVisibleFromTop = true;
-            var isVisibleFromBottom = true;
+    var treeHeight = treeGrid[row][col];
+    var visibleFromLeft = !Enumerable.Range(0, col).Any(leftIndex => treeGrid[row][leftIndex] >= treeHeight);
+    var visibleFromRight = !Enumerable.Range(col + 1, treeGrid[row].Count - col - 1).Any(rightIndex => treeGrid[row][rightIndex] >= treeHeight);
+    var visibleFromTop = !Enumerable.Range(0, row).Any(topIndex => treeGrid[topIndex][col] >= treeHeight);
+    var visibleFromBottom = !Enumerable.Range(row + 1, treeGrid.Count - row - 1).Any(bottomIndex => treeGrid[bottomIndex][col] >= treeHeight);
 
-            // check left
-            for (var leftIndex = j - 1; leftIndex >= 0; leftIndex--)
-            {
-                var height = treeGrid[i][leftIndex];
-                if (treeHeight <= height)
-                {
-                    isVisibleFromLeft = false;
-                    break;
-                }
-            }
-
-            // check top
-            for (var topIndex = i - 1; topIndex >= 0; topIndex--)
-            {
-                var height = treeGrid[topIndex][j];
-                if (treeHeight <= height)
-                {
-                    isVisibleFromTop = false;
-                    break;
-                }
-            }
-
-            // check right
-            for (var rightIndex = j + 1; rightIndex < treeGrid[i].Count; rightIndex++)
-            {
-                var height = treeGrid[i][rightIndex];
-                if (treeHeight <= height)
-                {
-                    isVisibleFromRight = false;
-                    break;
-                }
-            }
-
-            // check bottom
-            for (var bottomIndex = i + 1; bottomIndex < treeGrid.Count; bottomIndex++)
-            {
-                var height = treeGrid[bottomIndex][j];
-                if (treeHeight <= height) isVisibleFromBottom = false;
-            }
-
-            if (isVisibleFromBottom || isVisibleFromLeft || isVisibleFromRight || isVisibleFromTop)
-            {
-                visibleTreeCount++;
-            }
-        }
-    }
-
-    Console.WriteLine($"There are {visibleTreeCount} visible trees");
+    if (visibleFromBottom || visibleFromLeft || visibleFromRight || visibleFromTop) visibleTreeCount++;
 }
+
+Console.WriteLine($"There are {visibleTreeCount} visible trees");
+
+// Puzzle 2
+var highestScenicScore = 0;
+
+for (var row = 0; row < treeGrid.Count; row++)
+for (var col = 0; col < treeGrid[row].Count; col++)
+{
+    if (row == 0 || col == 0 || row == treeGrid.Count - 1 || col == treeGrid[row].Count - 1) continue; // skip edges
+    var treeHeight = treeGrid[row][col];
+
+    var leftTrees = treeGrid[row].Skip(0).Take(col).ToList();
+    var firstLeftBlockingTreeIdx = leftTrees.FindLastIndex(height => height >= treeHeight);
+    var viewCountFromLeft = firstLeftBlockingTreeIdx == -1 ? leftTrees.Count : col - firstLeftBlockingTreeIdx;
+
+    var rightTrees = treeGrid[row].Skip(col + 1).Take(treeGrid[row].Count - col - 1).Reverse().ToList();
+    var firstRightBlockingTreeIdx = rightTrees.FindLastIndex(height => height >= treeHeight);
+    var viewCountFromRight = firstRightBlockingTreeIdx == -1 ? rightTrees.Count : treeGrid[row].Count - col - firstRightBlockingTreeIdx - 1;
+
+    var topTrees = treeGrid.Skip(0).Take(row).Select(ints => ints[col]).ToList();
+    var firstTopBlockingTreeIdx = topTrees.FindLastIndex(height => height >= treeHeight);
+    var viewCountFromTop = firstTopBlockingTreeIdx == -1 ? topTrees.Count : row - firstTopBlockingTreeIdx;
+
+    var bottomTrees = treeGrid.Skip(row + 1).Take(treeGrid.Count - row - 1).Select(ints => ints[col]).Reverse().ToList();
+    var firstBottomBlockingTreeIdx = bottomTrees.FindLastIndex(height => height >= treeHeight);
+    var viewCountFromBottom = firstBottomBlockingTreeIdx == -1 ? bottomTrees.Count : treeGrid.Count - row - firstBottomBlockingTreeIdx - 1;
+
+    var scenicScore = viewCountFromLeft * viewCountFromRight * viewCountFromTop * viewCountFromBottom;
+    if (scenicScore > highestScenicScore) highestScenicScore = scenicScore;
+}
+
+Console.WriteLine($"HighestScenicScore is {highestScenicScore}");
 
 Console.WriteLine("Finished analysing tree grid.");
